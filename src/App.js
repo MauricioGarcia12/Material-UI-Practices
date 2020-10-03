8c839f8c40dceba1e7a93e63916bb8f9f32a3791
maumainsaw <maumainsaw@gmail.com>
2020-10-03 11:13:36 -0500

    Botones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import {Button,Typography} from '@material-ui/core'
+import {Grid,Button} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <Typography variant='h1' color='primary'>Hello there</Typography>
-      <Button variant='outlined' color='primary'>
-        El primer Boton
+      <Button color='primary' variant='outlined'>
+        outlined
+      </Button>
+      <br/>
+      <br/>
+      <Button color='primary' variant='contained'>
+        contained
+      </Button>
+      <br/>
+      <br/>
+      <Button color='primary' variant='contained' disableElevation>
+        disableElevation
+      </Button>
+      <br/>
+      <br/>
+      <Button color='primary' variant='contained' href='https://google.com'>
+        href
+      </Button>
+      <br/>
+      <br/>
+      <Button color='primary' variant='contained' fullWidth>
+        fullWidth
+      </Button>
+      <br/>
+      <br/>
+      <Button color='primary' variant='contained' size='small'>
+        size small
+      </Button>
+      <br/>
+      <br/>
+      <Button color='primary' variant='contained' size='medium'>
+        size medium
       </Button>
     </div>
   );
