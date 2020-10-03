83412a538c3fb335721d0df8768a5490f38decee
maumainsaw <maumainsaw@gmail.com>
2020-10-02 22:37:32 -0500

    Creando boton y personalizando Tipografias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button,Typography} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant='h1' color='primary'>Hello there</Typography>
+      <Button variant='outlined' color='primary'>
+        El primer Boton
+      </Button>
     </div>
   );
 }
